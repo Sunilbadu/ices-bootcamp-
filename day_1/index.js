ec69c83801bfcbe59fa32ae3fdb8aeeeb5d9649c
Sunilbadu <sunylbadu7@gmail.com>
2026-07-17 16:15:09 +0545

    day 1 part q

---
+++ day_1/index.js
@@ -0,0 +1,15 @@
+console.log("hello my name is sunil")
+let x = "iam robot"
+var y = "iam a bot"
+const z = "iam a person"
+
+//condition
+const number = 10;
+function showname(){
+    const name = "sunil";
+    console.log(name)
+    console.log(number)
+}
+console.log(name)
+
+showname();
