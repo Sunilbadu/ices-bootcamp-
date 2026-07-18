9c49f5acaf7c86f2c89445d1c12c19b0121787af
Sunilbadu <sunylbadu7@gmail.com>
2026-07-18 17:00:51 +0545

    objects

---
+++ day_2/object.js
@@ -0,0 +1,19 @@
+
+//objects
+
+
+const student  ={
+    name : "sunil",  //attrubutes
+    age : 20,
+    address :"kathmandu",
+    gender: "male",
+    // greet: function(){
+    //     console.log(`Hello ${this.name}`) //used this
+    // } //method
+    greet : ()=>{
+        console.log(`Hello ${student.name}`) //used this
+    }
+}
+student.greet()
+
+//arrow fun do not support this only locates the global variable
