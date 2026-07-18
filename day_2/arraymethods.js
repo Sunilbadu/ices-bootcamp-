b5bf6ea1175ba61876dee5db491af6ebec360f6c
Sunilbadu <sunylbadu7@gmail.com>
2026-07-18 16:32:18 +0545

    intreation methods

--- day_2/arraymethods.js
+++ day_2/arraymethods.js
@@ -4,12 +4,30 @@
 // 3. filter
 // 4. reduce 
  
+
+//for each method
 const studentList = ["ram", "hari" , "shyam" ,"gopal" ,"manish"]
 studentList.forEach((value) => {
     console.log("value:", value)
 });
     
-const newStudentList = studentList.map((value)=>{
-    return value + " wrc"
+
+//map
+const newStudentList = studentList.map((value , index)=>{
+    return value + " wrc" + " index:" + index
 })
 console.log(newStudentList)
+
+
+//filter nethod
+const studentsMarks =[10,20,30,40,50,60,70,80,90]
+const newFilter = studentsMarks.filter((value , index )=>{
+return value>=40;
+})
+console.log("passed:" , newFilter);
+
+//reduce method
+const newTotal = studentsMarks.reduce((total,value)=>{
+    return total + value
+})
+console.log("new total=", newTotal)
