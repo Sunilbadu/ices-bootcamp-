f89674fc31b3ea1eef46d9e85681a7dd777b1d3d
Sunilbadu <sunylbadu7@gmail.com>
2026-07-29 08:42:08 +0545

    add enter key event

--- todo/script.js
+++ todo/script.js
@@ -1,7 +1,17 @@
 const btn = document.getElementById("btn")
 const text = document.getElementById("texts")
-btn.onclick=()=>{
+function addTask() {
+    
     const tasks = document.createElement("p")
     tasks.className = "task"
-    document.getElementById("tasks").appendChild(task)
+    tasks.textContent = text.value
+    document.getElementById("tasks").appendChild(tasks)
+    text.value = ""
 }
+    
+btn.onclick = addTask();
+text.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
