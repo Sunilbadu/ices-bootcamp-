1626e8d441c92eb02b8f80cf146c9bc5d8fd70f9
Sunilbadu <sunylbadu7@gmail.com>
2026-07-29 08:30:23 +0545

    add key structure

---
+++ todo/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn")
+const text = document.getElementById("texts")
+btn.onclick=()=>{
+    const tasks = document.createElement("p")
+    tasks.className = "task"
+    document.getElementById("tasks").appendChild(task)
+}
